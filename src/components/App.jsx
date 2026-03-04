fddfc344f2ab9b5b46e3f16d51c783fe0caa21f4
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2026-03-05 00:38:09 +0500

    Update App component and enhance styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import Menubar from './ui/menubar/Menubar';
 import Gameboard from './ui/gameboard/Gameboard';
 import Stats from './ui/stats/Stats';
+import ResultCard from './ui/resultCard/ResultCard';
+import StartCard from './ui/startCard/StartCard';
 import './app.scss';
 
 export default function App() {
@@ -8,12 +10,13 @@ export default function App() {
     <main className="app">
       <div className="app-wrapper">
         <Menubar />
-
         <div className="game-wrapper">
           <Gameboard />
           <Stats />
         </div>
       </div>
+      {/* <StartCard /> */}
+      {/* <ResultCard /> */}
     </main>
   );
 }

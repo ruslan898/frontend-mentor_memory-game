c1b1f1acadf1926c9e36e7256c917f72f3056971
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2026-03-04 05:04:11 +0500

    Update App component and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,19 @@
+import Menubar from './ui/menubar/Menubar';
+import Gameboard from './ui/gameboard/Gameboard';
+import Stats from './ui/stats/Stats';
 import './app.scss';
 
 export default function App() {
-  return;
+  return (
+    <main className="app">
+      <div className="app-wrapper">
+        <Menubar />
+
+        <div className="game-wrapper">
+          <Gameboard />
+          <Stats />
+        </div>
+      </div>
+    </main>
+  );
 }

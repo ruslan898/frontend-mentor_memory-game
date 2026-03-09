76ab50647f2da25440449f42e9189e701f8414fc
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2026-03-10 03:59:07 +0500

    Add custom hooks for window width and utility function for time formatting

---
+++ src/components/hooks/useWindowWidth.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+
+export function useWindowWidth() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  function updateWindowWidth() {
+    setWindowWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', updateWindowWidth);
+    return () => window.removeEventListener('resize', updateWindowWidth);
+  });
+
+  return windowWidth;
+}
---
+++ src/utility/formatTime.js
@@ -0,0 +1,6 @@
+export function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+
+  return `${minutes}:${secs.toString().padStart(2, '0')}`;
+}
